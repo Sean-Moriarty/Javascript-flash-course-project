e59cdb747bded05d8dfb29b7fb78ca27aeade369
Sean Moriarty <sean@sean-m.net>
2023-10-13 20:07:29 -0700

    Started adding dynamic-ish greeting and weather info

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+// Menu Section
+
 // Open Menu
 document.querySelector("#open-nav-menu").addEventListener("click", function() {
     document.querySelector("header nav .wrapper").classList.add("nav-open");
@@ -8,3 +10,15 @@ document.querySelector("#open-nav-menu").addEventListener("click", function() {
 document.querySelector("#close-nav-menu").addEventListener("click", function() {
     document.querySelector("header nav .wrapper").classList.remove("nav-open");
 });
+
+
+// Greeting Section
+
+const greetingText = "Good Evening!";
+const weatherCondition = "clear";
+const userLocation = "Surprise";
+let temperature = 25.45420;
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${temperature.toFixed(1).toString()}°C outside.`;
+
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("p#weather").innerHTML = weatherText;
