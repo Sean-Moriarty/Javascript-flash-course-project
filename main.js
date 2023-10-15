de66d70c8cde9fbc8c230d7cc9a05973841f6212
Sean Moriarty <sean@sean-m.net>
2023-10-14 18:40:38 -0700

    Added celsius to fahrenheit button functionality

--- main.js
+++ main.js
@@ -14,11 +14,26 @@ document.querySelector("#close-nav-menu").addEventListener("click", function() {
 
 // Greeting Section
 
+function celsiusToFahr(temperature) {
+    let fahr = (temperature * 9 / 5) + 32;
+    return fahr;
+}
+
 const greetingText = "Good Evening!";
 const weatherCondition = "clear";
 const userLocation = "Surprise";
 let temperature = 25.45420;
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${temperature.toFixed(1).toString()}°C outside.`;
+let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${temperature.toFixed(1).toString()}°C outside.`;
+let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it’s ${celsiusToFahr(temperature).toFixed(1).toString()}°F outside.`;
 
 document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = weatherText;
+document.querySelector("p#weather").innerHTML = celsiusText;
+
+document.querySelector(".weather-group").addEventListener("click", function(e){
+    if (e.target.id == "celsius") {
+        document.querySelector("p#weather").innerHTML = celsiusText;
+    } else if (e.target.id == "fahr") {
+        document.querySelector("p#weather").innerHTML = fahrText;
+    }
+    
+});
